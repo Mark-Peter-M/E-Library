3d6be4d22b0e900510b9ac7744fd9c1d56841953
Mark-Peter-M <Maluumark7@gmail.com>
2022-06-27 18:36:50 +0300

    This is my second commit

--- JavaSc.js
+++ JavaSc.js
@@ -1,10 +1,14 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("header").style.top = "0";
-  } else {
-    document.getElementById("header").style.top = "-70px";
-  }
-  prevScrollpos = currentScrollPos;
+
+const f = document.getElementById('search');
+const q = document.getElementById('query');
+
+const site = 'https://b-ok.africa/s/';
+
+function submitted(event) {
+  event.preventDefault();
+  const url = site + q.value;
+  const win = window.open(url, '_blank');
+  win.focus();
 }
+f.addEventListener('submit', submitted);
+
