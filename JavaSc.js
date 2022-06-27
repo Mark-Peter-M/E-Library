55a7a78409db44c311df1d9eb8a2d2a5f01fd34d
Mark-Peter-M <Maluumark7@gmail.com>
2022-06-27 18:30:59 +0300

    Commit

--- JavaSc.js
+++ JavaSc.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-
-const f = document.getElementById('search');
-const q = document.getElementById('query');
-
-const site = 'https://b-ok.africa/s/';
-
-function submitted(event) {
-  event.preventDefault();
-  const url = site + q.value;
-  const win = window.open(url, '_blank');
-  win.focus();
-}
-f.addEventListener('submit', submitted);
-
-=======
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
@@ -24,4 +8,3 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
->>>>>>> 088b2077b0ca8cdc4b86c89625a46506233c4dd6
